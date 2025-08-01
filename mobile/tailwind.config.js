17012c4b09ccf870a0da05e082088c7bb5d09ce5
stackgik <uc@ucs-MacBook-Air.local>
2025-08-01 15:50:06 +0100

    config: configure the taliwind config file with project styles

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,12 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-  presets: [require('nativewind/preset')],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       fontFamily: {
-        'poppins-semibold': ['Poppins-SemiBold', 'san-serif'],
+        "poppins-semibold": ["Poppins-SemiBold", "san-serif"],
+      },
+      colors: {
+        "c-green": {
+          500: "#22c55e",
+          600: "#16a34a",
+          700: "#15803d",
+          800: "#166534",
+        },
+        "c-red": {
+          500: "#ef4444",
+          600: "#dc2626",
+          800: "#991b1b",
+        },
+        "c-blue": {
+          500: "#3b82f6",
+          600: "#2563eb",
+          800: "#1e40af",
+        },
+        "c-yellow": {
+          500: "#eab308",
+          600: "#ca8a04",
+        },
+        "c-amber": {
+          600: "#d97706",
+          700: "#b45309",
+        },
+        "c-indigo": {
+          500: "#a855f7",
+          600: "#9333ea",
+          800: "#6b21a8",
+        },
+        "c-orange": {
+          600: "#ea580c",
+        },
+        "c-neutral": {
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+        },
       },
     },
   },
