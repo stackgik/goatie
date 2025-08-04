7ad17f6c737ca018d5fca52cbc77b79134b75991
stackgik <uc@ucs-MacBook-Air.local>
2025-08-04 18:50:13 +0100

    refactor: reshape the goat document type and schema to fit the new UI design

--- backend/src/models/goat.models.ts
+++ backend/src/models/goat.models.ts
@@ -1,87 +1,141 @@
-import { intervalToDuration } from 'date-fns';
-import { Schema, Document, model } from 'mongoose';
-import { nanoid } from 'nanoid'; // Import nanoid
+import { intervalToDuration } from "date-fns";
+import { Schema, Document, Types, model } from "mongoose";
+import { nanoid } from "nanoid"; // Import nanoid
+
+interface Treatment {
+  treatmentId: string;
+  injectionName: string;
+  note: string;
+  date: Date;
+  veterinarian: string;
+  cost: number;
+}
 
 interface GoatDocument extends Document {
   goatId: string;
   goatName: string;
+  goatAge: string;
   image: string;
-  amountBought: number;
-  status: string;
-  dob: Date;
-  parent: Schema.Types.ObjectId;
+  weight: string;
+  purchasePrice: number;
+  purchaseDate: Date;
+  condition: string;
+  healthStatus: string;
+  lastHealthCheck: Date;
+  notes: string;
+  birthDate: Date;
+  parent: Types.ObjectId;
   gender: string;
-  numOfKids: number;
-  children: string[];
+  children: Types.ObjectId[];
   healthIssues: string[];
+  healthTreatments: Treatment[];
 }
+
+const treatmentSchema = new Schema<Treatment>({
+  treatmentId: {
+    type: String,
+    unique: true,
+    default: () => nanoid(8),
+  },
+  injectionName: {
+    type: String,
+    required: true,
+  },
+  note: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  veterinarian: {
+    type: String,
+    required: true,
+  },
+  cost: {
+    type: Number,
+    required: true,
+  },
+});
 
 const goatSchema = new Schema<GoatDocument>(
   {
     goatId: {
       type: String,
-      unique: true, // Ensure the ID is unique
-      default: () => nanoid(8), // Generate an 8-character alphanumeric ID
+      unique: true,
+      default: () => nanoid(8),
     },
     goatName: {
       type: String,
-      required: [true, 'Goat must have a name'],
+      required: [true, "Goat must have a name"],
       trim: true,
     },
     image: String,
-    amountBought: {
+    weight: {
+      type: String,
+      default: "undetermined",
+    },
+    purchasePrice: {
       type: Number,
       default: 0,
     },
-    status: {
+    purchaseDate: Date,
+    condition: {
       type: String,
       enum: {
-        values: ['alive', 'dead', 'sold', 'missing', 'stolen', 'consumed'],
-        message: '{VALUE} is not a valid status',
+        values: ["alive", "dead", "missing", "sold", "consumed", "stolen"],
+        message: "{VALUE} is not a valid goat condition",
       },
-      default: 'alive',
-      trim: true,
+      default: "alive",
     },
-    dob: {
+    healthStatus: {
+      type: String,
+      enum: {
+        values: ["healthy", "sick", "pregnant", "needs_attention"],
+        message: "{VALUE} is not a valid goat health status",
+      },
+    },
+    lastHealthCheck: Date,
+    notes: String,
+    birthDate: {
       type: Date,
-      required: [true, 'Goat must have a date of birth'],
+      required: [true, "Goat must have a date of birth"],
     },
     parent: {
       type: Schema.Types.ObjectId,
-      ref: 'Goat',
+      ref: "Goat",
     },
     gender: {
       type: String,
       enum: {
-        values: ['doe', 'buck'],
-        message: '{VALUE} is not a valid gender',
+        values: ["doe", "buck"],
+        message: "{VALUE} is not a valid gender",
       },
-      required: [true, 'Goat must have a gender'],
+      required: [true, "Goat must have a gender"],
     },
-    numOfKids: {
-      type: Number,
-      default: 0,
-    },
+
     children: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Goat',
+        ref: "Goat",
       },
     ],
     healthIssues: {
       type: [String],
       default: [],
     },
+    healthTreatments: [treatmentSchema],
   },
   {
     timestamps: true,
   }
 );
 
-goatSchema.virtual('age').set(function () {
+goatSchema.virtual("goatAge").set(function () {
   // This is a date-fns utility function that returns an object of paths e.g., {years, months, days, hours}
   const duration = intervalToDuration({
-    start: this.dob,
+    start: this.birthDate,
     end: new Date(),
   });
 
@@ -94,10 +148,10 @@ goatSchema.virtual('age').set(function () {
   if (duration.days && duration.days > 0) path.push(`${duration.days}d`);
 
   if (path.length === 0) {
-    return 'newborn';
+    return "newborn";
   }
 
-  return path.join('');
+  return path.join("");
 });
 
-export const Goat = model<GoatDocument>('Goat', goatSchema);
+export const Goat = model<GoatDocument>("Goat", goatSchema);
