e0ab5a5a4d4a34d4d5b72c2c2cc42f5142f780ee
stackgik <uc@ucs-MacBook-Air.local>
2025-08-22 12:28:40 +0100

    feat: create GoatCard to display small details about each goat

--- mobile/components/herd/goat_card.tsx
+++ mobile/components/herd/goat_card.tsx
@@ -1,84 +1,89 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import React from "react";
 import Badge from "../ui/badge";
+import { ActionTypes, GoatProps } from "@/types";
+import ActionButton from "./actions";
+import { Ionicons } from "@expo/vector-icons";
+import { actionButtonsConfig } from "@/data/mock/goat_profile.mock";
 
-const cardData = [
-  { id: 1, iconName: "barbell-outline", value: "155 lbs", title: "Weight" },
-  {
-    id: 2,
-    iconName: "heart-outline",
-    value: "Oct 22",
-    title: "last check",
-  },
-  {
-    id: 3,
-    iconName: "calendar-outline",
-    value: "2",
-    title: "treatments",
-  },
-];
+const GoatCard = ({ goat }: { goat: GoatProps }) => {
+  const {
+    goatName,
+    gender,
+    goatAge,
+    weight,
+    lastHealthCheck,
+    healthStatus,
+    notes,
+  } = goat || {};
 
-const GoatCard = () => {
   return (
-    <View className="bg-white rounded-[20px] p-4 gap-6">
-      {/* Top row */}
-      <View className="flex-row justify-between">
-        <View className="flex-row gap-2 items-center">
-          <View className="w-[80px] aspect-square rounded-lg overflow-hidden">
-            <Image
-              source={require("@/assets/images/splash.jpg")}
-              alt="goat image"
-              resizeMode="cover"
-              className="w-full h-full"
-            />
-          </View>
-
-          <View className="gap-1">
-            <Text className="font-plus_jakarta_sans-regular text-c-neutral-600 font-bold text-[16px]">
-              Bella
-            </Text>
-
-            <Text className="font-plus_jakarta_sans-regular text-c-neutral-500 font-semibold text-[12px]">
-              Doe . 3yrs 8mo
-            </Text>
-
-            <Badge healthStatus={"pregnant"} />
-          </View>
+    <TouchableOpacity
+      className="bg-white rounded-[20px] p-4 gap-6"
+      onPress={() => {}}
+      activeOpacity={0.7}
+    >
+      <View className="flex-row">
+        {/* The Goat Image container */}
+        <View className="w-[60px] aspect-square rounded-lg overflow-hidden">
+          <Image
+            source={require("@/assets/images/splash.jpg")}
+            alt="goat image"
+            resizeMode="cover"
+            className="w-full h-full"
+          />
         </View>
 
-        <TouchableOpacity className="relative" activeOpacity={0.7}>
-          <Ionicons name="ellipsis-vertical" size={20} color="#4b5563" />
-        </TouchableOpacity>
-      </View>
-
-      {/* Middle row */}
-      <View className="flex-row justify-between items-center">
-        {cardData.map((el) => (
-          <View className="gap-1 items-center" key={el.id}>
-            <Ionicons name={el.iconName as any} size={20} color="#9ca3af" />
-
-            <Text className="font-plus_jakarta_sans-regular text-c-neutral-600 font-bold text-[14px">
-              {el.value}
+        {/* The Goat Brief Details Container  */}
+        <View className="flex-1 ml-4 gap-2">
+          {/* The first Row */}
+          <View className="flex-row items-center justify-between">
+            <Text className="font-plus_jakarta_sans-bold text-c-neutral-600 font-bold text-[16px]">
+              {goatName}
             </Text>
 
-            <Text className="font-plus_jakarta_sans-regular text-c-neutral-400 font-bold text-[12px] capitalize">
-              {el.title}
-            </Text>
+            <Badge healthStatus={healthStatus} />
           </View>
-        ))}
+
+          {/* The Second Row */}
+          <Text className="font-plus_jakarta_sans-semibold text-c-neutral-500 text-[12px]">
+            {`${gender.charAt(0).toUpperCase() + gender.slice(1)} · ${goatAge}${goatAge > 1 ? "yrs" : "yr"} · ${weight}lbs`}
+          </Text>
+
+          {/* The Third Row */}
+          <Text className="font-plus_jakarta_sans-regular text-c-neutral-500 font-semibold text-base">
+            {notes}
+          </Text>
+
+          {/* The Fourth Row */}
+          <Text className="font-plus_jakarta_sans-regular text-c-neutral-500 font-semibold text-[12px]">{`Last Check: ${lastHealthCheck}`}</Text>
+        </View>
       </View>
 
-      {/* Action button */}
-      <TouchableOpacity
-        className=" py-3 rounded-md bg-c-green-600"
-        activeOpacity={0.6}
-      >
-        <Text className="text-c-green-100 font-plus_jakarta_sans-semibold capitalize text-center tracking-wide">
-          view details
-        </Text>
-      </TouchableOpacity>
-    </View>
+      <View className="flex-row justify-between items-center gap-3">
+        {actionButtonsConfig.map(
+          ({
+            id,
+            textColor,
+            bgColor,
+            title,
+            iconName,
+            iconColor,
+            actionType,
+          }) => (
+            <ActionButton
+              key={id}
+              textColor={textColor}
+              bgColor={bgColor}
+              title={title}
+              iconName={iconName as keyof typeof Ionicons.glyphMap}
+              iconColor={iconColor}
+              actionType={actionType as ActionTypes}
+              selectedGoat={goat}
+            />
+          )
+        )}
+      </View>
+    </TouchableOpacity>
   );
 };
 
